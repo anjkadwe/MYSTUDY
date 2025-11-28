21df2b061accae2d2aa48cf00da0d9aff165605f
anjkadwe <chhkadw18@gmail.com>
2025-11-28 23:16:27 +0530

    b

---
+++ study_PYTHON/a.py
@@ -0,0 +1,151 @@
+
+# i =1   # iteratoe  .....iteration
+# while i <= 5:
+#     print(i)
+
+#     i += 1
+
+# num = 1
+
+# while num <=100:
+#     print(num)
+#     num +=1   
+
+
+# i = 100
+# while i >=1:
+#     print(i)
+#     i -= 1
+
+# n = 1
+
+# while  n <=10 :
+#     print ( n* 2)
+#     n +=1
+
+# l = [ 1,4,9,16,25,36,49,64,81,100]
+ 
+# i = 0
+
+# while i < len(l):
+#  print(  "list index" , i, l[i] )
+  
+#  i +=1
+
+# l = ( 1,4,9,16,25,36,49,64,81,100)
+
+# x = 49  
+# i =0
+
+
+# while i < len( l ) :
+#     print("finding " ,i)
+
+#     if l[i] == x:
+#         print(i ,"found" , x)
+ 
+#     # print(i , l[i] )
+
+#     i += 1
+
+# heroes = [ "nano" , "mlay" ,"sora" ,"munya" ]
+
+# i = 0
+
+# while i < len (heroes) :
+#   print( heroes[i])
+
+#   i += 1
+
+
+
+# l = ( 1,4,9,16,25,36,49,64,81,100)
+
+# x = 49  
+# i =0
+
+
+
+# while i < len( l ) :
+#     print("finding " ,i)
+
+#     if l[i] == x:
+#         print(i ,"found" , x)
+#         break
+ 
+#     i  += 1
+
+
+# i = 0
+# veg = [ " potato" , "brinjal" , "ladyfinger" , "cucumber"]
+  
+# for val in veg:
+#     print(val )
+
+# string =" helloworld" 
+
+# for char in string:
+#     print(char)
+#     if char== "w":
+    
+#      print("fou")
+#     break
+
+# else   :
+#     print( "no")
+
+arry = [ 1,4,9,16,25,36,49,64,81,100]
+
+# for ele in arry:
+#     print(ele)
+
+# x= 25 
+
+# for ele in arry:
+#     print(  "finding",ele)
+
+#     if ele == x:
+#         print (ele)
+#         break
+
+# print( range(5))
+
+# for i in range(5):
+#     print( i)
+
+# for i in range(3 ,10):
+#     print( i)
+
+# for i in range(2 ,10, 3):
+#     print( i)
+
+# for i in range( 2 , 10 , 2):
+#     print( i)
+
+# for  i in range( 1 , 10 , 2):
+    # print( i )
+
+# for i in range( 1,101):
+#   print(i)
+
+
+# for i in range( 100, 0,-1):
+#   print(i)
+
+
+# for n in range (1,11,):
+
+#  print ( n *3 )
+
+
+n= 7
+
+# for i in range( 1 , n+1):
+#     n += i
+#     print( n)
+
+while n <= 5 :
+    n += 1
+    print  (  n  )
+
+    # n +=1
